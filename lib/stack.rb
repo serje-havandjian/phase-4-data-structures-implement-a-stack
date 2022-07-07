499147c7593a70e9cca7d87a3963478f8c285e29
Serje <Serje.havandjian@gmail.com>
2022-07-07 10:34:34 -0700

    commit tree fae24adeb44c147cde387e832fe9de37f8b866b8

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,36 @@
-# your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @stack.length > @limit
+            raise "Stack Overflow"
+        else 
+            @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.size = @limit ? true : false
+    end
+
+end
